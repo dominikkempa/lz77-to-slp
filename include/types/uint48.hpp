26d7ea6f24ea1b74c7b055ea8d43eb1d7e804e37
Dominik Kempa <dominik.kempa@gmail.com>
2024-08-14 01:22:55 +0200

    Fix a compiler warning about missing operator

--- include/types/uint48.hpp
+++ include/types/uint48.hpp
@@ -53,6 +53,13 @@ class uint48 {
     uint48(const std::int64_t& a) :
       low(a & 0xFFFFFFFFL), high((a >> 32) & 0xFFFF) {}
 
+    inline uint40& operator=(const uint40& x) {
+      if (this != &x) {
+        low = x.low;
+        high = x.high;
+      }
+      return *this;
+    }
     inline operator uint64_t() const {
       return (((std::uint64_t)high) << 32) | (std::uint64_t)low; }
     inline bool operator == (const uint48& b) const {
